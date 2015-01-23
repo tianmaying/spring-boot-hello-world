b68caba2c620686a95f5ab80cd85822a95a53e21
Ruici Luo <luoruici@gmail.com>
2015-01-23 17:59:31 +0800

    static templates.

--- src/main/java/com/tianmaying/app/Application.java
+++ src/main/java/com/tianmaying/app/Application.java
@@ -4,16 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @EnableAutoConfiguration
 @Controller
 public class Application {
 
     @RequestMapping("/")
-    @ResponseBody
     public String index() {
-        return "Hello World!";
+        return "index";
     }
 
     public static void main(String[] args) {
